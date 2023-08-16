0ceda3cb3ae5cfd07e9c1d97b1275ced1d609892
sorinM2 <131362539+sorinM2@users.noreply.github.com>
2023-08-16 11:15:17 +0300

    OpenGL Setup

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,7 +1,13 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
 
 int main(void)
 {
+
+
     GLFWwindow* window;
 
     /* Initialize the library */
@@ -10,6 +16,9 @@ int main(void)
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+
+    
     if (!window)
     {
 
@@ -20,6 +29,9 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error! Glew failed to initialize.\n";
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
